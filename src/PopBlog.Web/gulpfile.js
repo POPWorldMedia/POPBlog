1f2b9d8054e88f17154b56357682e120b4969bfb
Jeff Putz <jeff@popw.com>
2020-06-14 16:49:11 -0400

    bootstrap and front-end build

---
+++ src/PopBlog.Web/gulpfile.js
@@ -0,0 +1,43 @@
+﻿/// <binding AfterBuild="default" />
+
+var gulp = require("gulp"),
+	merge = require("merge-stream"),
+	babel = require("gulp-babel"),
+	cssmin = require("gulp-cssmin"),
+	uglify = require("gulp-uglify"),
+	sourcemaps = require("gulp-sourcemaps"),
+	rename = require("gulp-rename");
+
+var nodeRoot = "./node_modules/";
+var targetPath = "./wwwroot/lib/";
+
+gulp.task("copies", function () {
+	var streams = [
+		gulp.src(nodeRoot + "bootstrap/dist/**/*").pipe(gulp.dest(targetPath + "/bootstrap/dist")),
+		gulp.src(nodeRoot + "popper.js/dist/umd/popper.min.js").pipe(gulp.dest(targetPath + "/popper.js/dist")),
+		gulp.src(nodeRoot + "jquery/dist/**/*").pipe(gulp.dest(targetPath + "/jquery/dist")),
+		gulp.src(nodeRoot + "tinymce/**/*").pipe(gulp.dest(targetPath + "/tinymce"))
+	];
+	return merge(streams);
+});
+
+gulp.task("js", function () {
+	return gulp.src("./wwwroot/lib/PopBlog/src/*.js")
+		.pipe(sourcemaps.init())
+		.pipe(babel({ presets: ["@babel/preset-env"], sourceMap: true }))
+		.pipe(uglify())
+		.pipe(rename({ suffix: '.min' }))
+		.pipe(sourcemaps.write("./"))
+		.pipe(gulp.dest(targetPath + "/PopBlog/dist"));
+});
+
+gulp.task("css", function () {
+	return gulp.src("./wwwroot/lib/PopBlog/src/*.css")
+		.pipe(cssmin())
+		.pipe(rename({ suffix: '.min' }))
+		.pipe(gulp.dest(targetPath + "/PopBlog/dist"));
+});
+
+gulp.task("min", gulp.series(["copies", "js", "css"]));
+
+gulp.task("default", gulp.series("min"));
