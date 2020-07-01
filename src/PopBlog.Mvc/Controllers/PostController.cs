a798834cfff66510eb55e43cb0dd3c1ed0caf096
Jeff Putz <jeff@popw.com>
2020-07-01 11:47:37 -0400

    post detail

---
+++ src/PopBlog.Mvc/Controllers/PostController.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PopBlog.Mvc.Services;
+
+namespace PopBlog.Mvc.Controllers
+{
+	public class PostController : Controller
+	{
+		private readonly IPostService _postService;
+
+		public PostController(IPostService postService)
+		{
+			_postService = postService;
+		}
+
+		[HttpGet("/blog/{urlTitle}")]
+		public async Task<IActionResult> Detail(string urlTitle)
+		{
+			var post = await _postService.Get(urlTitle);
+			if (post == null)
+				return StatusCode(404);
+			return View(post);
+		}
+	}
+}
